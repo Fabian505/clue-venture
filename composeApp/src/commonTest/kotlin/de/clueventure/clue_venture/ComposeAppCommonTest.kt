65b7d056961c09dc59b9415a7152612089a20a3a
Dariusz Herzog <i23015@hb.dhbw-stuttgart.de>
2026-04-03 13:58:15 +0200

    feat: add app setup

---
+++ composeApp/src/commonTest/kotlin/de/clueventure/clue_venture/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package de.clueventure.clue_venture
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/de/clueventure/clue_venture/MainViewController.kt
@@ -0,0 +1,5 @@
+package de.clueventure.clue_venture
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
